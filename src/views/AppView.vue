d0e3556c73e1d85520ce186f3e53f9fb577e284b
ckjr3000 <charlotte.kj.roe@gmail.com>
2023-11-07 10:35:59 +0000

    reimplemented overlay

--- src/views/AppView.vue
+++ src/views/AppView.vue
@@ -1,9 +1,20 @@
 <template>
-    <canvas v-if="showCanvas" ref="canvas"></canvas>
+    <canvas 
+        v-if="showCanvas" 
+        ref="canvas"
+        @mousedown="handleOverlayMouseDown"
+        @mousemove="handleOverlayMouseMove"
+        @mouseup="handleOverlayMouseUp"
+        @touchstart="handleOverlayTouchStart"
+        @touchmove="handleOverlayTouchMove"
+        @touchend="handleOverlayTouchEnd"
+        >
+    </canvas>
     <div v-if="!uploadedImageUrl">
       <p>Upload a selfie to get started:</p>
       <button @click="handleUploadClick">Upload Image</button>
     </div>
+    <!-- edit buttons -->
     <div v-if="uploadedImageUrl">
         <button v-if="editValue > 0 || editType === 'glitch'" @click="handleApplyChanges">Apply Changes</button>
         <button v-if="appliedImageData" @click="handleUndo">Undo</button>
@@ -28,7 +39,24 @@
         <input v-if="editType === 'saturation'" type="range" min="0" max="10" step="0.5" @change="handleValueChange">
 
         <button value="glitch" @click="handleGlitch">Glitch!</button>
+
+        <button value="overlay" @click="handleOverlay">Overlay</button>
     </div>
+
+    <!-- overlay menu -->
+    <div v-if="editType === 'overlay'">
+        <p>Select an overlay:</p>
+        <button v-if="isOverlaySelected" @click="saveCanvas">Save Image</button>
+        <div id="overlay-options">
+            <img
+            v-for="overlay in overlayOptions"
+            :key="overlay.id"
+            :src="overlay.src"
+            @click="selectOverlay(overlay)"
+            />
+      </div>
+    </div>
+
 </template>
 
 <script>
@@ -48,11 +76,181 @@ export default {
             currentVersionIndex: -1,
             redoAvailable: false,
             changesApplied: false,
+            overlayOptions: [
+                {
+                id: 1,
+                src: require('@/assets/overlays/1.png')
+                },
+                {
+                id: 2,
+                src: require('@/assets/overlays/2.png')
+                },
+                {
+                id: 3,
+                src: require('@/assets/overlays/3.png')
+                },
+                {
+                id: 4,
+                src: require('@/assets/overlays/4.png')
+                },
+                {
+                id: 5,
+                src: require('@/assets/overlays/5.png')
+                },
+                {
+                id: 6,
+                src: require('@/assets/overlays/6.png')
+                },
+                {
+                id: 7,
+                src: require('@/assets/overlays/7.png')
+                },
+                {
+                id: 8,
+                src: require('@/assets/overlays/8.png')
+                },
+                {
+                id: 9,
+                src: require('@/assets/overlays/9.png')
+                },
+                {
+                id: 10,
+                src: require('@/assets/overlays/10.png')
+                },
+                {
+                id: 11,
+                src: require('@/assets/overlays/11.png')
+                },
+                {
+                id: 12,
+                src: require('@/assets/overlays/12.png')
+                },
+                {
+                id: 13,
+                src: require('@/assets/overlays/13.png')
+                },
+                {
+                id: 14,
+                src: require('@/assets/overlays/14.png')
+                },
+                {
+                id: 15,
+                src: require('@/assets/overlays/15.png')
+                },
+                {
+                id: 16,
+                src: require('@/assets/overlays/16.png')
+                },
+                {
+                id: 17,
+                src: require('@/assets/overlays/17.png')
+                },
+                {
+                id: 18,
+                src: require('@/assets/overlays/18.png')
+                },
+                {
+                id: 19,
+                src: require('@/assets/overlays/19.png')
+                },
+                {
+                id: 20,
+                src: require('@/assets/overlays/20.png')
+                },
+                {
+                id: 21,
+                src: require('@/assets/overlays/21.png')
+                },
+                {
+                id: 22,
+                src: require('@/assets/overlays/22.png')
+                },
+                {
+                id: 23,
+                src: require('@/assets/overlays/23.png')
+                },
+                {
+                id: 24,
+                src: require('@/assets/overlays/24.png')
+                },
+                {
+                id: 25,
+                src: require('@/assets/overlays/25.png')
+                },
+                        {
+                id: 26,
+                src: require('@/assets/overlays/26.png')
+                },
+                        {
+                id: 27,
+                src: require('@/assets/overlays/27.png')
+                },
+                {
+                id: 28,
+                src: require('@/assets/overlays/28.png')
+                },
+                        {
+                id: 29,
+                src: require('@/assets/overlays/29.png')
+                },
+                {
+                id: 30,
+                src: require('@/assets/overlays/30.png')
+                },
+                {
+                id: 31,
+                src: require('@/assets/overlays/31.png')
+                },
+                {
+                id: 32,
+                src: require('@/assets/overlays/32.png')
+                },
+                {
+                id: 33,
+                src: require('@/assets/overlays/33.png')
+                },
+                {
+                id: 34,
+                src: require('@/assets/overlays/34.png')
+                },
+                {
+                id: 35,
+                src: require('@/assets/overlays/35.png')
+                },
+                {
+                id: 36,
+                src: require('@/assets/overlays/36.png')
+                },
+                {
+                id: 37,
+                src: require('@/assets/overlays/37.png')
+                },
+                {
+                id: 38,
+                src: require('@/assets/overlays/38.png')
+                },
+                {
+                id: 39,
+                src: require('@/assets/overlays/39.png')
+                },
+            ],
+            isOverlaySelected: false,
+            overlayPosition: { x: 0, y: 0 },
+            overlaySize: { width: 500, height: 500 },
+            touchStartPos: null,
+            offscreenCanvas: null,
+            offscreenContext: null,
+            isScaling: false,
+            scalingStartPos: { x: 0, y: 0 },
+            isMoving: false,
+            movingStartPos: { x: 0, y: 0 },
         }
     },
     mounted(){
         this.canvas = this.$refs.canvas;
         this.retrieveSessionId();
+        this.offscreenCanvas = document.createElement('canvas');
+        this.offscreenContext = this.offscreenCanvas.getContext('2d')
     },
     methods: {
         retrieveSessionId(){
@@ -196,6 +394,153 @@ export default {
             };
 
             glitchImage(canvas, glitchParams);
+        },
+        handleOverlay(){
+            this.editType = 'overlay';
+        },
+        selectOverlay(overlay){
+            this.overlayImageUrl = overlay.src;
+            this.overlayImage = new Image();
+            this.overlayImage.src = overlay.src;
+            this.isOverlaySelected = true;
+            this.drawCanvas();
+            this.isScaling = false;
+            this.scalingStartPos = { x: 0, y: 0 };
+        },
+        drawCanvas() {
+            const canvas = this.$refs.canvas;
+            const context = canvas.getContext("2d");
+
+            this.offscreenCanvas.width = canvas.width;
+            this.offscreenCanvas.height = canvas.height;
+            this.offscreenContext.clearRect(0, 0, canvas.width, canvas.height);
+
+            const img = new Image();
+            img.crossOrigin="Anonymous"
+            img.onload = () => {
+            this.offscreenContext.drawImage(img, 0, 0);
+
+            if (this.isOverlaySelected && this.overlayImage) {
+                const width = this.overlaySize.width;
+                const height = this.overlaySize.height;
+                const x = this.overlayPosition.x + this.overlayImage.width / 2 - width / 2;
+                const y = this.overlayPosition.y + this.overlayImage.height / 2 - height / 2;
+
+                this.offscreenContext.drawImage(
+                this.overlayImage,
+                x,
+                y,
+                width,
+                height
+                );
+            }
+
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            context.drawImage(this.offscreenCanvas, 0, 0);
+            };
+            img.src = this.uploadedImageUrl;
+        },
+        handleOverlayMouseDown(event) {
+            const canvas = this.$refs.canvas;
+            const rect = canvas.getBoundingClientRect();
+            const x = event.clientX - rect.left;
+            const y = event.clientY - rect.top;
+
+            // Check for shift + click for scaling
+            if (event.shiftKey) {
+                // Start scaling
+                this.isScaling = true;
+                this.scalingStartSize = { width: this.overlaySize.width, height: this.overlaySize.height };
+                this.scalingStartPos = { x, y };
+            } else {
+                // Start moving
+                this.isMoving = true;
+                this.movingStartPos = { x, y };
+                this.movingStartOverlayPos = { x: this.overlayPosition.x, y: this.overlayPosition.y };
+            }
+        },
+        handleOverlayMouseMove(event) {
+            const canvas = this.$refs.canvas;
+            const rect = canvas.getBoundingClientRect();
+            const x = event.clientX - rect.left;
+            const y = event.clientY - rect.top;
+
+            if (this.isScaling && this.overlayImage) {
+            const scaleRatioX = (x - this.scalingStartPos.x) / 100;
+            const scaleRatioY = (y - this.scalingStartPos.y) / 100;
+
+            // Scale while maintaining aspect ratio
+            if (event.shiftKey) {
+                const scaleRatio = Math.max(scaleRatioX, scaleRatioY);
+                this.overlaySize.width = this.scalingStartSize.width + scaleRatio * this.overlayImage.width;
+                this.overlaySize.height = this.scalingStartSize.height + scaleRatio * this.overlayImage.height;
+            } else {
+                this.overlaySize.width = this.scalingStartSize.width + scaleRatioX * this.overlayImage.width;
+                this.overlaySize.height = this.scalingStartSize.height + scaleRatioY * this.overlayImage.height;
+            }
+
+            this.drawCanvas();
+            } else if (this.isMoving && this.overlayImage) {
+            const deltaX = x - this.movingStartPos.x;
+            const deltaY = y - this.movingStartPos.y;
+
+            this.overlayPosition.x = this.movingStartOverlayPos.x + deltaX;
+            this.overlayPosition.y = this.movingStartOverlayPos.y + deltaY;
+
+            this.drawCanvas();
+            }
+        },
+        handleOverlayMouseUp() {
+            this.isScaling = false;
+            this.isMoving = false;
+        },
+        handleOverlayTouchStart(e) {
+            const touch = e.touches[0];
+            if (e.touches.length === 1) {
+                // Single touch for moving the overlay
+                this.handleOverlayMouseDown(touch);
+            } else if (e.touches.length === 2) {
+                // Two touches for scaling the overlay
+                this.handlePinchStart(e);
+            }
+        },
+        handleOverlayTouchMove(e) {
+            const touch = e.touches[0];
+            if (e.touches.length === 1) {
+            // Single touch for moving the overlay
+            this.handleOverlayMouseMove(touch);
+            } else if (e.touches.length === 2) {
+            // Two touches for scaling the overlay
+            this.handlePinchMove(e);
+            }
+        },
+        handleOverlayTouchEnd(e) {
+            if (e.touches.length === 0) {
+            // No touches for ending scaling
+            this.handleOverlayMouseUp();
+            }
+        },
+        handlePinchStart(e) {
+            const touch1 = e.touches[0];
+            const touch2 = e.touches[1];
+            this.pinchStartDistance = this.getDistance(touch1, touch2);
+            this.pinchStartSize = { width: this.overlaySize.width, height: this.overlaySize.height };
+        },
+        handlePinchMove(e) {
+            const touch1 = e.touches[0];
+            const touch2 = e.touches[1];
+            const pinchDistance = this.getDistance(touch1, touch2);
+
+            const scaleRatio = pinchDistance / this.pinchStartDistance;
+            this.overlaySize.width = this.pinchStartSize.width * scaleRatio;
+            this.overlaySize.height = this.pinchStartSize.height * scaleRatio;
+
+            this.drawCanvas();
+        },
+        getDistance(point1, point2) {
+            const dx = point1.clientX - point2.clientX;
+            const dy = point1.clientY - point2.clientY;
+            return Math.sqrt(dx * dx + dy * dy);
         },
         handleApplyChanges() {
             const canvasData = this.$refs.canvas.toDataURL("image/jpeg");
