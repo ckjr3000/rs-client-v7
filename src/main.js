0145a38065b0ea29b08aade553f688247ce4c628
ckjr3000 <charlotte.kj.roe@gmail.com>
2023-11-13 10:33:32 +0000

    imported font awesome icons

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faBars);
 
 createApp(App)
     .use(router)
-    //.component('font-awesome-icon', FontAwesomeIcon)
+    .component('font-awesome-icon', FontAwesomeIcon)
     .mount('#app')
